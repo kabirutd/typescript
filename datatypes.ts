0d126839edc2a0fbeedbb3e98de867dffc4bebf1
kabirutd <mohammadkabir@gmail.com>
2019-10-02 14:22:14 -0500

    10022019

---
+++ datatypes.ts
@@ -0,0 +1,20 @@
+let fullName2: string = "Mohammad Kabir";
+let age2: number = 35;
+
+let nameAge2 = "My name is ${fullName2} and my age will be ${age2+1} next year.";
+console.log(nameAge2);
+
+//arrays and filter set of arrays
+let arr1: number[] = [3, 1, 3, 5, 2, 4, 4, 4];
+/*
+let users: string[] = [
+    { id: 11, name: 'Adam', age: 23, group: 'editor' },
+    { id: 47, name: 'John', age: 28, group: 'admin' },
+    { id: 85, name: 'William', age: 34, group: 'editor' },
+    { id: 97, name: 'Oliver', age: 28, group: 'admin' }
+];
+*/
+
+
+//let uniqueArray: number[] = [...new Set(arr1)];
+//console.log("uniqueArray[1]: " + uniqueArray[1]);
